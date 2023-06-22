958f111f1fd462a0aee68eef78c115ec05dad251
vishnu <vishnu981203@gmail.com>
2023-06-22 11:30:37 +0530

    add header section

--- components/home/header.jsx
+++ components/home/header.jsx
@@ -1,22 +1,28 @@
 import Image from "next/image";
 import React from "react";
+import { HiPencilSquare } from "react-icons/hi2";
 
 const Header = () => {
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex justify-between p-5 border-b-2">
         <Image src="/assets/logo.jpg" width={80} height={50} alt="home logo" />
-        <div>
-          <button className=" bg-cyan-400 rounded-md">Create</button>
-          <button>Sign In</button>
+        <div className="flex justify-between items-center p-2 gap-4">
+          <button className=" bg-cyan-400 rounded-md px-6 py-0 h-10 text-white">
+            <span className="hidden sm:block">Create</span>
+            <HiPencilSquare className="sm:hidden" />
+          </button>
+          <button className="border-2 border-gray-300 px-4 py-0 h-10 rounded-xl">
+            Sign In
+          </button>
+          <Image
+            src="/assets/profile.jpg"
+            width={40}
+            height={10}
+            alt="profile logo"
+            className="rounded-full cursor-pointer"
+          />
         </div>
-        <Image
-          src="/assets/profile.jpg"
-          width={40}
-          height={20}
-          alt="profile logo"
-          className="rounded-full"
-        />
       </div>
     </>
   );
