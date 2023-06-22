277e86e76b2a7d19ccc3dc7cb08a027f5c8540b1
vishnu <vishnu981203@gmail.com>
2023-06-22 14:33:51 +0530

    config next auth

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
 import Footer from "@/components/home/footer";
 import Header from "@/components/home/header";
+import { SessionProvider } from "next-auth/react";
 
-export default function Home() {
+export default function Home({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <>
-      <Header />
-      <div className="text-2xl bg-orange-500">welcome!</div>
-      <Footer />
+      <SessionProvider session={session}>
+        <Header />
+        <div className="text-2xl bg-orange-500">welcome!</div>
+        <Footer />
+      </SessionProvider>
     </>
   );
 }
--- components/home/header.jsx
+++ components/home/header.jsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import { HiPencilSquare } from "react-icons/hi2";
+import { HiPencilSquare, HiArrowLeftOnRectangle } from "react-icons/hi2";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
+  console.log("Session", session);
   return (
     <>
       <div className="flex justify-between p-5 border-b-2">
@@ -10,10 +13,11 @@ const Header = () => {
         <div className="flex justify-between items-center p-2 gap-4">
           <button className=" bg-cyan-400 rounded-md px-6 py-0 h-10 text-white">
             <span className="hidden sm:block">Create</span>
-            <HiPencilSquare className="sm:hidden" />
+            <HiPencilSquare className="sm:hidden text-[20px]" />
           </button>
           <button className="border-2 border-gray-300 px-4 py-0 h-10 rounded-xl">
-            Sign In
+            <span className="hidden sm:block"> Sign In</span>
+            <HiArrowLeftOnRectangle className="sm:hidden text-[20px]" />
           </button>
           <Image
             src="/assets/profile.jpg"
