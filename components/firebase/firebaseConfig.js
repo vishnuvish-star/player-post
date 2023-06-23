69422246fd714c42ce53aa188f35b4949fca436e
vishnu <vishnu981203@gmail.com>
2023-06-23 13:27:57 +0530

    install and config firebase

---
+++ components/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDv7a8FjcgnR6fJr60_LeGooHNoUS6EzKY",
+  authDomain: "player-post-a8dcd.firebaseapp.com",
+  projectId: "player-post-a8dcd",
+  storageBucket: "player-post-a8dcd.appspot.com",
+  messagingSenderId: "913801615910",
+  appId: "1:913801615910:web:758905ccff6bad2a02e4e7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- components/home/searchBar.jsx
+++ components/home/searchBar.jsx
@@ -1,15 +1,27 @@
+"use client";
+
 import React from "react";
 
 const SearchBar = () => {
   return (
     <>
+      <div className=" m-10">
+        <h2 className="text-2xl w-1/2 mx-auto text-blue-400 mb-5">
+          Find & Explore games and activity..
+        </h2>
+        <h2 className="font-bold w-1/2 mx-auto">Best entertainment here..</h2>
+      </div>
+
       <form className="flex justify-center mt-10 ">
         <input
           type="text"
           placeholder="Search Posts..."
-          className="input input-ghost w-full max-w-xs p-2 border-[2px] border-gray-300 rounded-lg"
+          className="input w-full max-w-xs p-2 border-gray-300 rounded-lg outline-none"
+          onChange={(text) => console.log(text.target.value)}
         />
-        <button className="btn btn-primary"></button>
+        <button className="btn w-[6rem] bg-cyan-400 text-white ml-2">
+          Find
+        </button>
       </form>
     </>
   );
